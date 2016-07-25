4324da623cad56d8f8435977d799f763d1743ab9
Julen Pardo <julen.pardo@outlook.es>
2016-07-25 15:25:37 +0200

    dev-wp_installable: add required php stuff for installation

---
+++ tinymce-table-of-contents.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Plugin Name: TinyMCE Table of Contents
+ * Version: 1.0
+ * Plugin URI: https://github.com/julenpardo/tinymce-table-of-contents
+ * Author: Julen Pardo
+ * Author URI: https://github.com/julenpardo
+ * Description: Completely automated and instantaneous way of generating tables of contents for posts.
+ */
+
+class TinyMCE_Table_of_Contents {
+
+    const VERSION = '1.0';
+
+    /**
+     * TinyMCE_Table_of_Contents class constructor.
+     */
+    public function __construct() {
+    	if ( is_admin() ) {
+    		add_action( 'init', array( $this, 'setup_tinymce_plugin' ) );
+    	}
+    }
+
+    /**
+     * Initializes the plugin.
+     */
+    public function setup_tinymce_plugin() {
+			
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+			        return;
+		}
+
+		if ( get_user_option( 'rich_editing' ) !== 'true' ) {
+		return;
+		}
+
+		add_filter( 'mce_external_plugins', array( $this, 'add_tinymce_plugin' ) );
+		add_filter( 'mce_buttons', array( $this, 'add_tinymce_toolbar_button' ) );
+    }
+
+    public function add_tinymce_plugin( $plugin_array ) {
+    	$plugin_array['table_of_contents'] = plugin_dir_url( __FILE__ ) . '/js/table-of-contents.js';
+
+    	return $plugin_array;
+    }
+
+    public function add_tinymce_toolbar_button( $buttons ) {
+    	array_push( $buttons, '|', 'table_of_contents' );
+
+    	return $buttons;
+    }
+}
+
+$tinymce_table_of_contents = new TinyMCE_Table_of_Contents();
+
+?>
